2c85f7ea70a7eab684b923279345c781bd964980
Alex Soares Duarte <r0b07z3r0@gmail.com>
2022-04-21 22:18:00 -0300

    defineFlows

--- writePLCOpen.py
+++ writePLCOpen.py
@@ -1,4 +1,7 @@
 from parsePNML import ET, getET, Net, Page, Place, Transition, Arc, runPNMLParse
+import re
+
+flowlist = []
 
 class POU:
 
@@ -37,9 +40,39 @@ class Steps:
     
     
     
-def definePOU():
-    print("Defining POUs")
+def defineFlows():
+    print("Defining Flows")
     
+    #The file must be inputed by user on app GUI
+    #A pop-up must ask for the struct file
+    file = open('pequenaFabricaInterpretada_wAUTONET-struct.txt')
+    lines = file.readlines()
+    file.close()
+    for line in lines:
+        print('line index:'+ str(lines.index(line)) + ' line:' + line)
+    
+    psemiflowindex = lines.index('P-SEMI-FLOWS GENERATING SET ------------------------------------- \n')
+    tsemiflowindex = lines.index('T-SEMI-FLOWS GENERATING SET ------------------------------------- \n')
+    print(str(lines.index('P-SEMI-FLOWS GENERATING SET ------------------------------------- \n')))
+    print(str(lines.index('T-SEMI-FLOWS GENERATING SET ------------------------------------- \n')))
+    
+    flowslines = lines[psemiflowindex+4:tsemiflowindex-3]
+    
+    testflow = 'p0 p1 {p lugar} {p3 place} p4 (1)\n'
+    
+    flowslines.append(testflow)
+    
+    print(flowslines)
+    
+    
+    for flows in flowslines:
+        flows = re.sub('(\{)(.*?)\s(.*?)(\})', r'\2_\3', flows)
+        flows = re.sub('\(.*?\n+', '', flows)
+        flows = flows.strip()
+        flowlist.append(flows.split(' '))
+ 
+    print(flowlist)
+        
 
 def writePLCOpen():
     print("Writing PLCopen file")
@@ -48,7 +81,7 @@ def writePLCOpen():
 
 def main():
     
-    definePOU()
+    defineFlows()
     writePLCOpen()
     
 if __name__ == "__main__":
